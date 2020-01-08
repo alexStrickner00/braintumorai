e620f07d98215b1cc122e6383209eeae0ae9bc10
Alexander Strickner <alexstrickner00@gmail.com>
2020-01-08 12:39:06 +0100

    initial

---
+++ braintumorAI/src/main/java/handler/IndexHandler.java
@@ -0,0 +1,10 @@
+package handler;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "indexHandler")
+@SessionScoped
+public class IndexHandler {
+	
+}
