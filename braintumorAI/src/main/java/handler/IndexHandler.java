1b707a2c5dba48ed96a7c8e591e423cd4098a113
Alexander Strickner <alexstrickner00@gmail.com>
2020-01-15 14:47:16 +0100

    finished

--- braintumorAI/src/main/java/handler/IndexHandler.java
+++ braintumorAI/src/main/java/handler/IndexHandler.java
@@ -2,7 +2,6 @@ package handler;
 
 import java.io.IOException;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
@@ -12,10 +11,8 @@ import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
 import org.deeplearning4j.nn.modelimport.keras.exceptions.InvalidKerasConfigurationException;
 import org.deeplearning4j.nn.modelimport.keras.exceptions.UnsupportedKerasConfigurationException;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
-import org.deeplearning4j.util.ModelSerializer;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
-import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 
 import lombok.Getter;
@@ -27,11 +24,13 @@ public class IndexHandler {
 	@Getter
 	@Setter
 	private UploadedFile uploadedFile;
+	@Getter
+	@Setter
+	private double result;
 	
-	public void handleFileUpload(FileUploadEvent event) {
-        FacesMessage msg = new FacesMessage("Successful", event.getFile().getFileName() + " is uploaded.");
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-        compute();
+	public void handleFileUpload() {
+		if(uploadedFile != null)
+			compute();
         
     }
 	
@@ -41,10 +40,12 @@ public class IndexHandler {
 			try {
 				model = KerasModelImport.importKerasSequentialModelAndWeights(this.getClass().getResourceAsStream("model.hdf5"));
 				NativeImageLoader loader = new NativeImageLoader(150, 150, 3);
-				INDArray input = loader.asMatrix(uploadedFile);
+				INDArray input = loader.asMatrix(uploadedFile.getInputstream());
 		        ImagePreProcessingScaler preProcessor = new ImagePreProcessingScaler(0, 1);
 		        preProcessor.transform(input);
-				model.output(input, false);
+		        result = model.output(input).getDouble(0,0);
+		        System.out.println(model.predict(input));
+		        FacesContext.getCurrentInstance().getExternalContext().redirect("result.xhtml");
 			} catch (InvalidKerasConfigurationException | UnsupportedKerasConfigurationException e) {
 				e.printStackTrace();
 			}
@@ -52,5 +53,9 @@ public class IndexHandler {
 			e.printStackTrace();
 		}
 
+	}
+	
+	public boolean isTumor() {
+		return result > 0.6;
 	}
 }
